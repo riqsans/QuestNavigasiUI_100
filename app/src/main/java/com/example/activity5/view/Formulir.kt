681f1eef2cf49c2c364cd492eb5bba1c379bccc1
riqsans <thrq88@gmail.com>
2025-11-06 13:25:03 +0700

    Menambahkan TopAppBar pada halaman Formulir

--- app/src/main/java/com/example/activity5/view/Formulir.kt
+++ app/src/main/java/com/example/activity5/view/Formulir.kt
@@ -4,19 +4,31 @@ package com.example.activity5.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.activity5.R
 
 @Composable
 fun FormIsian(
-    jenisK:List<String> = listof("Laki-laki", "Perempuan"),
+    jenisK:List<String> = listOf("Laki-laki", "Perempuan"),
     OnSubmitBtnClick:() -> Unit
 ){
-    Scaffold (modifier = Modifier,
+    Scaffold(modifier = Modifier,
         {
             TopAppBar(
-
+                title = {Text(stringResource(id= R.string.home),
+                    color = Color.White)},
+                colors = TopAppBarDefaults.topAppBarColors(
+                    colorResource(id = R.color.teal_700)
+                )
             )
-        })
+        }){ isiRuang ->
+
+    }
 }

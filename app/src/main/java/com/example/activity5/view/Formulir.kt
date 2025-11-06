7a1fae696fd7b8aa7ba4194787e8e15f42c94a9e
riqsans <thrq88@gmail.com>
2025-11-06 13:30:09 +0700

    menambahkan field nama lengkap

--- app/src/main/java/com/example/activity5/view/Formulir.kt
+++ app/src/main/java/com/example/activity5/view/Formulir.kt
@@ -2,16 +2,23 @@
 
 package com.example.activity5.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.activity5.R
 
 @Composable
@@ -29,6 +36,20 @@ fun FormIsian(
                 )
             )
         }){ isiRuang ->
+        Column (modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally)
+        {
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(250.dp),
+                label = {Text(text = "Nama Lengkap")},
+                onValueChange = { },
+            )
+        }
 
     }
 }

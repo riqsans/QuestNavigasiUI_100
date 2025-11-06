e013f2d55ffdbc71abab6307238b70209b5256fa
riqsans <thrq88@gmail.com>
2025-11-06 13:48:07 +0700

    membuat file TampilData.kt

---
+++ app/src/main/java/com/example/activity5/view/TampilData.kt
@@ -0,0 +1,8 @@
+package com.example.activity5.view
+
+import androidx.compose.runtime.Composable
+
+@OptIn(=ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    onBlackBtnClick: () -> Unit)

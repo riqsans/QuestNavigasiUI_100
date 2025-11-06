41fc639fe85069c5c6e6e7b4cdc1e271787033dd
riqsans <thrq88@gmail.com>
2025-11-06 13:09:43 +0700

    membuat top app bar

--- app/src/main/java/com/example/activity5/Navigasi.kt
+++ app/src/main/java/com/example/activity5/Navigasi.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -43,14 +44,8 @@ fun DataApp(
     }
 }
 
-private fun cancelAndBackToFormulirku(navController: NavHostController) {
-    navController.popBackStack(Navigasi.Formulirku,name,
-        inlcusive = false)
-}
-
-@Composable
-fun TampilData(onBlackBtnClick: () -> cancelAndBackToFormulirku) {
-    TODO("Not yet implemented")
+private fun cancelAndBackToFormulirku(navController: NavController){
+    navController.popBackStack(Navigasi.Formulirku.name, inclusive = false)
 }
 
 
--- app/src/main/java/com/example/activity5/view/Formulir.kt
+++ app/src/main/java/com/example/activity5/view/Formulir.kt
@@ -1,8 +1,19 @@
 package com.example.activity5.view
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun FormIsian(){
+fun FormIsian(
+    jenisK:List<String> = listof("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick:() -> Unit
+){
+    Scaffold (modifier = Modifier,
+        {
+            TopAppBar(
 
+            )
+        })
 }
